a222252466a3cd0cc4eab99a234e5666731f69c0
jwywoo <jwywoo26@gmail.com>
2024-12-09 10:31:21 +0900

    JOING-53 feat: 서버분리(#37)
    ### As is
    - AI Server
      - Proposal
      - Channel
      - Rec System
    ### To be
    - AI Rec Server
      - Rec System
    - AI Gen Server
      - Proposal
      - Channel

--- src/config.py
+++ src/config.py
@@ -6,19 +6,15 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: Optional[str] = None
-    YOUTUBE_DATA_API_KEY: Optional[str] = None
 
     AWS_REGION: str = "ap-northeast-2"
-    PARAMETER_NAME_OPENAI: str = "/joing/ai/openai-key"
-    PARAMETER_NAME_YOUTUBE: str = "/joing/ai/youtube-data-key"
+    PARAMETER_NAME_OPENAI: str = "/joing/ai/openai-rec-key"
 
     def __init__(self):
         super().__init__()
-        if not self.OPENAI_API_KEY and not self.YOUTUBE_DATA_API_KEY:
+        if not self.OPENAI_API_KEY:
             self.OPENAI_API_KEY = self.get_parameter_from_aws(
                 self.PARAMETER_NAME_OPENAI)
-            self.YOUTUBE_DATA_API_KEY = self.get_parameter_from_aws(
-                self.PARAMETER_NAME_YOUTUBE)
 
     def get_parameter_from_aws(self, parameter_name: str) -> str:
         try:
--- src/main.py
+++ src/main.py
@@ -5,8 +5,6 @@ from fastapi.responses import JSONResponse
 from starlette import status
 
 # Routers import
-from proposal.router import router as proposal_router
-from channel.router import router as channel_router
 from rec_system.router import router as rec_router
 
 # config
@@ -14,8 +12,6 @@ from .config import settings
 
 # app init & router
 app = FastAPI()
-app.include_router(channel_router)
-app.include_router(proposal_router)
 app.include_router(rec_router)
 
 
@@ -30,6 +26,7 @@ def health_check():
         status_code=status.HTTP_200_OK,
         content={"status": "ok", "message": "Service is ready"}
     )
+
 
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, exc: Exception):

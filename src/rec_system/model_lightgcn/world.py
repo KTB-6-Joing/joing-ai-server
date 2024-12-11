879374f3cd5f723fd3acd059775d767a695985d2
0like2 <dudfkr236@gmail.com>
2024-12-11 18:34:32 +0900

    fix: world.py 수정
    - config path 수정

--- src/rec_system/model_lightgcn/world.py
+++ src/rec_system/model_lightgcn/world.py
@@ -26,10 +26,10 @@ def initialize_world():
 
     # Define paths
     ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-    INPUT_PATH = join(ROOT_PATH, 'input')
-    OUTPUT_PATH = join(ROOT_PATH, 'output')
-    BOARD_PATH = join(OUTPUT_PATH, 'runs')
-    FILE_PATH = join(OUTPUT_PATH, 'checkpoints')
+    INPUT_PATH = join(ROOT_PATH, '/rec_system/model_lightgcn/input')
+    OUTPUT_PATH = join(ROOT_PATH, '/rec_system/model_lightgcn/output')
+    BOARD_PATH = join(OUTPUT_PATH, '/rec_system/model_lightgcn/runs')
+    FILE_PATH = join(OUTPUT_PATH, '/rec_system/model_lightgcn/output/checkpoints')
 
     # Ensure necessary directories exist
     if not os.path.exists(FILE_PATH):

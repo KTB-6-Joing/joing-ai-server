eb6ca988a64b8c9e91edab08dd4ada019b5188c4
0like2 <dudfkr236@gmail.com>
2024-12-15 06:30:32 +0900

    fix: config 파일 및 neumf 재학습 추가, 데이터셋 통일, datatype 변경 반영
    - media_type : _type 붙이는걸로 수정
    - score : int에서 float으로 변경

--- src/rec_system/model_lightgcn/dataloader.py
+++ src/rec_system/model_lightgcn/dataloader.py
@@ -61,7 +61,7 @@ class SimilarityDataset(BasicDataset):
         self.item_category_mapping = dict(enumerate(self.items['item_category'].astype("category").cat.categories))
         self.channel_category_mapping = dict(
             enumerate(self.creators['channel_category'].astype("category").cat.categories))
-        self.media_type_mapping = {0: 'short', 1: 'long'}
+        self.media_type_mapping = {0: 'short_type', 1: 'long_type'}
 
         self._similarity_matrix = pd.read_csv(similarity_matrix_file, index_col=0).values
 
@@ -101,8 +101,8 @@ class SimilarityDataset(BasicDataset):
     def _validate_items(self, items):
         items['title'] = items['title'].fillna('unknown')
         items['item_category'] = items['item_category'].fillna('unknown').astype("category").cat.codes
-        items['media_type'] = items['media_type'].fillna('unknown').map({'short': 0, 'long': 1}).fillna(0).astype(int)
-        items['score'] = items['score'].fillna(0).astype(int)
+        items['media_type'] = items['media_type'].fillna('unknown').map({'short_type': 0, 'long_type': 1}).fillna(0).astype(int)
+        items['score'] = items['score'].fillna(0).astype(float)
         items['item_content'] = items['item_content'].fillna('unknown')
         return items
 
